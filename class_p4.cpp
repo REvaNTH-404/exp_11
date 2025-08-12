6630b05aa2bcd211324b1e7aed8b9c50ac0f47fb
REvaNTH-404 <revanthsai.work@gmail.com>
2025-08-12 15:23:15 +0530

    Add files via upload

---
+++ class_p4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class cube{
+    public:
+    int height;
+    int width;
+    int length;
+
+    void input(){
+    cout << "Enter value for height: ";
+    cin >> height;
+    cout << "Enter value for width: ";
+    cin >> width;
+    cout << "Enter value for length: ";
+    cin >> length;
+    }
+
+int volume(){
+    int v;
+    v = height*width*length ;
+    return v;
+    }
+};
+
+int main(){
+   cube cube1;
+   cube1.input();
+    int vol = cube1.volume() ;
+    cout<<"volume:"<<vol<<endl;
+}
